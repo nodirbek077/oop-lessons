3353679f8f8f2f2015aa0628e85cc8177be88281
Nodirbek <n.nurkulov@insuranceon.uz>
2026-01-20 11:15:49 +0500

    wrapper class understood in detail

--- src/wrapperclasses/autoboxing/Main.java
+++ src/wrapperclasses/autoboxing/Main.java
@@ -7,17 +7,45 @@ public class Main {
 //        //autoboxing: primitive tipdagi o'zgaruvchilarning non primitiv tipga o'zgarishiga aytiladi
 
          //shuni teskarisiga unboxing deyiladi ya'ni wrapper classlardan primitive tiplarga o'tiush jarayoni
-        Integer m = 51;
-        test(m);
-    }
+//        Integer m = 51;
+//        test(m);
 
-    public static void print(Integer n){
-        System.out.println(n);
-    }
+        int a = 2;
+        byte b = 12;
 
-    public static void test(int n){
-        System.out.println(n);
+        Integer a1 = new Integer(a);
+        Byte b1 = new Byte(b);
+        Double c = 3.14;
+        Character d = 'F';
+        short e = 14;
+        Float f = 9.9f;
+        Boolean g = true;
+        Boolean h = false;
+        Boolean t = new Boolean(true);
+        Long l = 166L;
+        Integer k = 90;
+
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c.doubleValue());
+        System.out.println(d.charValue());
+        System.out.println(Short.valueOf(e));
+        System.out.println(f.floatValue());
+        System.out.println(g);
+        System.out.println(h.booleanValue());
+        System.out.println(l.longValue());
+        System.out.println(t);
+        String kStr = k.toString();
+        System.out.println(Integer.valueOf(kStr) + 90);
     }
+//
+//    public static void print(Integer n){
+//        System.out.println(n);
+//    }
+//
+//    public static void test(int n){
+//        System.out.println(n);
+//    }
 
 
 }
