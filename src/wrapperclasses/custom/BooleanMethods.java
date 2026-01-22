6b5a437ab697e1598677fddac6e90c820689cf07
Nodirbek <n.nurkulov@insuranceon.uz>
2026-01-22 18:00:04 +0500

    Boolean method has been learned partly

---
+++ src/wrapperclasses/custom/BooleanMethods.java
@@ -0,0 +1,13 @@
+package wrapperclasses.custom;
+
+public class BooleanMethods {
+    public static void main(String[] args) {
+        Boolean b1 = false;
+        Integer num = 91;
+
+        Boolean b2 = new Boolean(false);
+        System.out.println(b2.equals(b1));
+
+
+    }
+}
