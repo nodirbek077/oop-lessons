c0dccc0d0d5ffc23fe0ac96c2ff9ef496a189edb
Nodirbek <n.nurkulov@insuranceon.uz>
2026-01-26 14:09:40 +0500

    getUniversityName and createSubject methods has been created

--- university-manager-oop-module-1/src/Main.java
+++ university-manager-oop-module-1/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*UniversityManager university = new UniversityManager("University 1");
+        System.out.println(university.getUniversityName());
+
+        UniversityManager university2 = new UniversityManager("University 2");
+        System.out.println(university2.getUniversityName());*/
+
+        UniversityManager university = new UniversityManager("University 1");
+        Subject subjectMath = university.createSubject("Math", 2);
+        Subject subjectHistory = university.createSubject("History", 2);
+        Subject subjectJava = university.createSubject("Java", 2);
+
+        Subject subject = university.createSubject("Java", 2);
+        System.out.println(subject);
     }
 }
